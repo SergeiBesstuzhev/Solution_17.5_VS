97e5797d9481431d2f8cb5538d6e6732f32a7f84
Sergei Bess <sergeibesstuzhev1989@gmail.com>
2023-08-12 09:02:12 +0300

    Добавьте файлы проекта.

---
+++ Solution_17.5_VS.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "Math.h"
+
+class Vector {
+public:
+    Vector() : x(5), y(5), z(5) {}
+    Vector(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+    void ShowVector() {
+        std::cout << "Vector: " << x << ", " << y << ", " << z;
+    }
+    void ShowVectorLength() {
+        std::cout << "Vector length: " << sqrt((pow(x, 2) + pow(y, 2) + pow(z, 2)));
+    }
+private:
+    float x;
+    float y;
+    float z;
+};
+
+int main()
+{
+    Vector V;
+    V.ShowVector();
+    V.ShowVectorLength();
+}
